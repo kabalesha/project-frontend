74ff837cf6a05685ebd996909d91b1b3ed8303e5
VirLg <19virlg@gmail.com>
2023-11-11 19:10:41 +0200

    add

--- src/components/month/Month.jsx
+++ src/components/month/Month.jsx
@@ -2,25 +2,29 @@ import React from 'react';
 import MonthItem from './MonthItem';
 
 const Month = () => {
-  const month = [
-    {
-      January: '31',
-      February: [28, 29],
-      March: '31',
-      April: '31',
-      May: '31',
-      June: '31',
-      July: '31',
-      August: '31',
-      September: '31',
-      October: '31',
-      November: '31',
-      December: '31',
-    },
+  const date = new Date().getFullYear();
+
+  const year = [
+    '31',
+    date % 4 === 0 ? 29 : 28,
+    '31',
+    '30',
+    '31',
+    '30',
+    '31',
+    '31',
+    '30',
+    '31',
+    '30',
+    '31',
   ];
+
+  const currentMonth = new Date().getMonth();
+  const quantityDays = year[currentMonth];
+
   return (
     <div>
-      <MonthItem month={month} />
+      <MonthItem quantityDays={quantityDays} />
     </div>
   );
 };
--- src/components/month/MonthItem.jsx
+++ src/components/month/MonthItem.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const MonthItem = ({ month }) => {
-  month.map(el => console.log('el', el));
-  return <div>MonthItem</div>;
+const MonthItem = ({ quantityDays }) => {
+  const i = [];
+  let n = 0;
+  while (n < 30) {
+    n++;
+    i.push(n);
+  }
+  console.log('i', i);
+  return i.map(el => (
+    <div key={el} style={{ border: '1px solid red', radius: '30px' }}>
+      {el}
+    </div>
+  ));
 };
 
 export default MonthItem;
