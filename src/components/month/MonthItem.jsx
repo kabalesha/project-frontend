f26c7cb36d1301fbaba0323ae3c63f40855f8eb6
VirLg <19virlg@gmail.com>
2023-11-12 00:38:14 +0200

    add

--- src/components/month/MonthItem.jsx
+++ src/components/month/MonthItem.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 
-const MonthItem = ({ quantityDays }) => {
-  const i = [];
-  let n = 0;
-  while (n < 30) {
-    n++;
-    i.push(n);
-  }
-  console.log('i', i);
-  return i.map(el => (
-    <div
-      key={el}
-      style={{
-        border: '1px solid red',
-        borderRadius: '20px',
-        width: '34px',
-        height: '34px',
-        alignItems: 'center',
-
-        gridTemplateColumns: 'repeat(3, 1fr)',
-      }}
-    >
-      {el}
+const MonthItem = ({ quantityDays, value }) => {
+  const handleClick = el => {
+    console.log('i', el);
+    alert(el);
+  };
+  return i.map((el, idx) => (
+    <div style={{ display: 'block' }}>
+      <div
+        onClick={() => handleClick(idx)}
+        key={el}
+        style={{
+          display: 'inherit',
+          backgroundColor: 'blue',
+          borderRadius: '20px',
+          width: '34px',
+          height: '34px',
+          alignItems: 'center',
+          marginBottom: '8px',
+        }}
+      >
+        {el}
+      </div>
+      <span style={{ fontSize: '12px' }}>{value} %</span>
     </div>
   ));
 };
