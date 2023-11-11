0df4c433a3e6267358b01a359aefeaf2be713513
VirLg <19virlg@gmail.com>
2023-11-11 14:35:27 +0200

    add

--- src/components/today/Today.jsx
+++ src/components/today/Today.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Today = () => {
-  return <div>Today</div>;
+  const a = [
+    { id: 1, portion: 250, time: '7:00' },
+    { id: 2, portion: 200, time: '9:00' },
+    { id: 3, portion: 250, time: '11:00' },
+  ];
+
+  return a.map(el => {
+    return (
+      <div key={el.id}>
+        <div>{el.portion}</div>:<div>{el.time}</div>
+      </div>
+    );
+  });
 };
 
 export default Today;
