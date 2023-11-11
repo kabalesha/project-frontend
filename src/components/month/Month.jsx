9468d25458476e0903667c98c4643a18a1fe8173
VirLg <19virlg@gmail.com>
2023-11-11 15:01:56 +0200

    add month

--- src/components/month/Month.jsx
+++ src/components/month/Month.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import MonthItem from './MonthItem';
 
 const Month = () => {
-  return <div>Month</div>;
+  const month = [
+    {
+      January: '31',
+      February: [28, 29],
+      March: '31',
+      April: '31',
+      May: '31',
+      June: '31',
+      July: '31',
+      August: '31',
+      September: '31',
+      October: '31',
+      November: '31',
+      December: '31',
+    },
+  ];
+  return (
+    <div>
+      <MonthItem month={month} />
+    </div>
+  );
 };
 
 export default Month;
---
+++ src/components/month/MonthItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MonthItem = ({ month }) => {
+  return <div>MonthItem</div>;
+};
+
+export default MonthItem;
