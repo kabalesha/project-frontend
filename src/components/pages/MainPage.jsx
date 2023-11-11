ee1ef43925428863bdb74b42e12bb9a4d0869080
VirLg <19virlg@gmail.com>
2023-11-11 13:38:58 +0200

    add

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import css from "./MainPage.module.css"
+import React from 'react';
+import css from './MainPage.module.css';
 const MainPage = () => {
   return (
-    <div className={css.mainPageContainer}><div className={css.heroImg}></div>MainPage</div>
-  )
-}
+    <div className={css.mainPageContainer}>
+      <div className={css.heroImg}>IMG</div>MainPage
+    </div>
+  );
+};
 
-export default MainPage
+export default MainPage;
