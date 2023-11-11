4a9d1a4b1d9ee97df3ca1cc4dcdd3304f1972a58
VirLg <19virlg@gmail.com>
2023-11-11 20:13:15 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MainPage from '../src/components/pages/MainPage';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MainPage />
     </div>
   );
 }
