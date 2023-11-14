a9d0f4d26492dccac6249681e20000327c22ba00
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-11-14 22:27:49 +0100

    add fonts

--- src/components/header/AuthNav/AuthNav.jsx
+++ src/components/header/AuthNav/AuthNav.jsx
@@ -1,22 +1,17 @@
-import React from "react";
-import icon from "./outline.png"
-import css from "./authNav.module.css"
-
+import React from 'react';
+import icon from './outline.png';
+import css from './authNav.module.css';
 
 const AuthNav = () => {
-
   return (
     <>
-      
-      <button type='button' className={css.signIn_btn}>
-        
-        <span className={css.signInText}>Sign in</span> 
-        
-      <img className={css.user_icon} src={icon} alt="User icon"></img>
-</button>
-      
-      </>
-  )
+      <button type="button" className={css.signIn_btn}>
+        <span className={css.signInText}>Sign in</span>
+
+        <img className={css.user_icon} src={icon} alt="User icon"></img>
+      </button>
+    </>
+  );
 };
 
 export default AuthNav;
