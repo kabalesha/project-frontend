d1763f7f97451a640bd955b05a1b14c1f108d4c9
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-11-14 18:01:06 +0100

    fix errors

--- src/App.js
+++ src/App.js
@@ -1,50 +1,45 @@
-
-
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import MainPage from './components/pages/homePage/HomePage.jsx';
+// import Login from '../pages/Login';
+import SignUpPage from './components/pages/SignUp/SignUpPage.jsx';
+import Main from './components/main/Main.jsx';
+import Header from './components/header/Header';
 
 import './App.css';
 
 import HomePage from './components/pages/homePage/HomePage';
 
 function App() {
-
-
   return (
     <div className="App">
-      
       {/* <Header /> */}
       <HomePage />
 
       {/* <SignUpPage /> */}
     </div>
+  );
+}
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import MainPage from './components/pages/MainPage.jsx';
-// import Login from '../pages/Login';
-import SignUpPage from './components/pages/SignUp/SignUpPage.jsx'
-import Main from './components/main/Main.jsx';
-import Header from './components/header/Header';
-
-
-const AppRouter = () => {
+export const AppRouter = () => {
   return (
-<>
-    <Header/>
-    <Router>
-      <Routes>
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route index element={<Navigate to="/Main" />} />
-        <Route path="/SignUpPage" element={<SignUpPage />} />
-        <Route path="/MainPage" element={<MainPage />} />
-        <Route path="/Main" element={<Main />} />
-      </Routes>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/Main" />} />
+          <Route path="/SignUpPage" element={<SignUpPage />} />
+          <Route path="/MainPage" element={<MainPage />} />
+          <Route path="/Main" element={<Main />} />
+        </Routes>
       </Router>
-      </>
-
+    </>
   );
 };
 
-
 export default App;
-
-export default AppRouter;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import css from "./header.module.css"
-import logoPNG from '../../icons/Logo.png'
+// import { useDispatch } from 'react-redux';
+import css from './header.module.css';
+import logoPNG from '../../icons/Logo.png';
 
 const Header = () => {
   return (
-    <div >
+    <div>
       <header className={css.headerContainer}>
-        <button className={css.logo_btn} type='button'>
-           <img className={css.logo} src={logoPNG} alt="Logo" />
+        <button className={css.logo_btn} type="button">
+          <img className={css.logo} src={logoPNG} alt="Logo" />
         </button>
-        
-       <button type='button' className={css.signIn_btn}>
-         <span className={css.signInText}>Sign in</span> 
-         <svg className={css.round_icon} width="28" height="28" >
-        <use href="../../icons/icons.svg#user-icon"></use>
-      </svg>
 
+        <button type="button" className={css.signIn_btn}>
+          <span className={css.signInText}>Sign in</span>
+          <svg className={css.round_icon} width="28" height="28">
+            <use href="../../icons/icons.svg#user-icon"></use>
+          </svg>
         </button>
       </header>
-      </div>
-  )
+    </div>
+  );
 };
 
 export default Header;
