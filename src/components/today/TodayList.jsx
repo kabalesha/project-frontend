58d37eaaeeb5b642f66a81cfc748c09d9cc47ad9
VirLg <19virlg@gmail.com>
2023-11-14 23:00:25 +0200

    add

--- src/components/today/TodayList.jsx
+++ src/components/today/TodayList.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { quantityDrinkSelector } from '../../redux/selectors';
 
-const TodayList = () => {
+const TodayList = ({ remoteDataPortion }) => {
   const drinkingList = useSelector(quantityDrinkSelector);
 
   return ([] && drinkingList).map((el, idx) => {
     return (
       <div key={idx} style={{ display: 'flex', justifyContent: 'center' }}>
         <div>{el.time}</div>_______<div>{el.portion}</div>
-        <button
-          onClick={() => {
-            console.log(idx);
-          }}
-        >
-          Remove
-        </button>
+        <button onClick={() => remoteDataPortion(idx)}>Remove</button>
         <button>Del</button>
       </div>
     );
