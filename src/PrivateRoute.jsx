b2baf3dc78ea34fd352d8908bff44723dd52083c
VirLg <19virlg@gmail.com>
2023-11-16 19:09:38 +0200

    drop

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -2,14 +2,17 @@ import { useSelector } from 'react-redux';
 import SignInPage from './components/pages/SignIn/SignInPage';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  const isRefreshing = useSelector(state => state.auth.isRefreshing);
+  // const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  // const isRefreshing = useSelector(state => state.auth.isRefreshing);
 
-  return !isLoggedIn && !isRefreshing ? (
-    <SignInPage to={redirectTo} />
-  ) : (
-    <Component />
-  );
+  // const isToken = localStorage.getItem('token');
+  // console.log('isToken', isToken);
+  // console.log(5);
+  // return !isLoggedIn && !isRefreshing ? (
+  //   <SignInPage to={redirectTo} />
+  // ) : (
+  return <Component />;
+  // );
 };
 
 export default PrivateRoute;
---
+++ src/components/drop/Drop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Drop = () => {
+  return (
+    <div class="dropdown">
+      <button class="dropbtn">Dropdown</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+  );
+};
+
+export default Drop;
