7abeb8147515f35142b3912a95b01d48f22dba2f
NesterovSerhii <sergei_nesterov8@ukr.net>
2023-11-20 14:14:09 +0200

    sum

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
+import * as Yup from 'yup'
 import { register } from '../../shared/api/auth';
 import { useNavigate, Link } from 'react-router-dom';
-import {
-  RegisterButton,
-  Label,
-  Input,
-  Form,
-  Title,
-  SignInLink,
-} from '../../components/auth/AuthForm.styled';
+
+
+import css from './AuthForm.module.css'
+import icons from '../../icons/icons.svg'
+
 import { signUpThunk } from '../../redux/auth/thunkUser';
+
 
 const AuthForm = ({ onSubmit }, handleFormSubmit) => {
   console.log('first', handleFormSubmit);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -33,44 +35,66 @@ const AuthForm = ({ onSubmit }, handleFormSubmit) => {
             password: values.password,
           })
         );
-        // handleFormSubmit({
-        // name:values.name
-        //   email: values.email,
-        //   password: values.password,
-        // });
+        handleFormSubmit({
+          name:values.name,
+          email: values.email,
+          password: values.password,
+        });
 
         formik.resetForm();
         if (onSubmit) {
           onSubmit(values);
         }
 
-        navigate('/HomePage');
+        navigate('/home');
       } catch (error) {
-        console.error('Не вдалий вхід', error);
+        console.error('Registration failed', error);
       }
     },
+      validationSchema: Yup.object().shape({
+      email: Yup.string().email('Invalid email').required('Email is required'),
+      password: Yup.string()
+        .required('Password is required')
+          .min(8, 'Password must be at least 8 characters long')
+          .max(64, 'Password must not exceed 64 characters'),
+      repeatPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Repeat password is required'),
+    }),
   });
+
+
+   const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleRepeatPasswordVisibility = () => {
+    setShowRepeatPassword(!showRepeatPassword);
+  };
+
 
   return (
     <div>
-      <Title>Sign up</Title>
-      <Form onSubmit={formik.handleSubmit}>
+      <h1 className={css.title}>Sign up</h1>
+      <form className={css.form} onSubmit={formik.handleSubmit}>
         <div>
-          <Label>Enter your email</Label>
-          <Input
+          <label className={css.label}>Enter your email</label>
+          <input className={`${css.input} ${formik.touched.email && formik.errors.email ? css.inputError : ''}`}
             type="email"
             name="email"
             placeholder="E-mail"
             minLength={3}
-            autoComplete="off"
             required
             {...formik.getFieldProps('email')}
           />
+          {formik.touched.email && formik.errors.email && (
+            <div className={css.error}>{formik.errors.email}</div>
+          )}
         </div>
         <div>
-          <Label>Enter your password</Label>
-          <Input
-            type="password"
+          <label className={css.label}>Enter your password</label>
+          <input className={`${css.input} ${formik.touched.password && formik.errors.password ? css.inputError : ''}`}
+            type={showPassword ? 'text' : 'password'}
             name="password"
             placeholder="Password"
             autoComplete="off"
@@ -78,11 +102,27 @@ const AuthForm = ({ onSubmit }, handleFormSubmit) => {
             required
             {...formik.getFieldProps('password')}
           />
+          <div className={css.passwordToggle} onClick={togglePasswordVisibility}>
+              
+          {showPassword ? (
+                          <svg className={css.eye}>
+                             <use href={icons + '#eye'} />
+                          </svg>
+                        ) : (
+                          <svg className={css.eye}>
+                            <use href={icons + '#hidden'} />
+                          </svg>
+                        )}
+          </div>
+          
+          {formik.touched.password && formik.errors.password && (
+            <div className={css.error}>{formik.errors.password}</div>
+          )}
         </div>
         <div>
-          <Label>Repeat password</Label>
-          <Input
-            type="password"
+          <label className={css.label}>Repeat password</label>
+          <input className={`${css.input} ${formik.touched.repeatPassword && formik.errors.repeatPassword ? css.inputError : ''}`}
+            type={showRepeatPassword ? 'text' : 'password'}
             name="password"
             placeholder="Repeat password"
             autoComplete="off"
@@ -90,12 +130,24 @@ const AuthForm = ({ onSubmit }, handleFormSubmit) => {
             required
             {...formik.getFieldProps('repeatPassword')}
           />
+          <div className={css.passwordToggle} onClick={toggleRepeatPasswordVisibility}>
+          {showRepeatPassword ? (
+                          <svg className={css.eye}>
+                             <use href={icons + '#eye'} />
+                          </svg>
+                        ) : (
+                          <svg className={css.eye}>
+                            <use href={icons + '#hidden'} />
+                          </svg>
+                        )}
+          </div>
+          {formik.touched.repeatPassword && formik.errors.repeatPassword && (
+            <div className={css.error}>{formik.errors.repeatPassword}</div>
+          )}
         </div>
-        <RegisterButton type="submit">Sign Up</RegisterButton>
-        <SignInLink>
-          <Link to="/signin">Sign in</Link>
-        </SignInLink>
-      </Form>
+        <button className={css.btn} type="submit">Sign Up</button>
+        <Link className={css.link} to="/signin">Sign in</Link>
+      </form>
     </div>
   );
 };
