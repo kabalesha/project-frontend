00a2d81a704d6a6801e49f54527247f77d46dc52
Iryna Husieva <02guseva02@gmail.com>
2023-11-21 17:58:37 +0100

    sum

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import { Route, Routes, Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/layout/Layout.jsx';
-// import Main from './components/main/Main.jsx';
 import WelcomePage from './components/pages/WelcomePage/WelcomePage.jsx';
 import PrivateRoute from './PrivateRoute.jsx';
 import RestrictedRoute from './RestrictedRoute.jsx';
 import { lazy } from 'react';
 
 const HomePage = lazy(() => import('./components/pages/homePage/HomePage.jsx'));
-const SignInPage = lazy(() =>
-  import('./components/pages/SignIn/SignInPage.jsx')
-);
-const SignUpPage = lazy(() =>
-  import('./components/pages/SignUp/SignUpPage.jsx')
-);
+const SignInPage = lazy(() => import('./components/pages/SignIn/SignInPage.jsx'));
+const SignUpPage = lazy(() => import('./components/pages/SignUp/SignUpPage.jsx'));
 
 const App = () => {
   return (
