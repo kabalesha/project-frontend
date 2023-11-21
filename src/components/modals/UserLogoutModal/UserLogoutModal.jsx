00b8eaed9151c244d1d3b0da7b7cde492d4e8ceb
AnnPilchak <anya.pilchak.03@gmail.com>
2023-11-21 21:58:20 +0200

    modal

---
+++ src/components/modals/UserLogoutModal/UserLogoutModal.jsx
@@ -0,0 +1,30 @@
+import css from "./UserLogoutModal.module.css";
+import icon from './../../../icons/icons.svg'
+
+const UserLogoutModal = () => {
+  return (
+    <>
+      <div className={css.backdrop}>
+        <div className={css.modal_wrapper}>
+          <button type="button" className={css.modal_close_btn}>
+            <svg width="24px" height="24px">
+              <use href={icon + '#close'}></use>
+            </svg>
+          </button>
+            <h1 className={css.modal_title}>Log out</h1>
+            <h2 className={css.modal_subtitle}>Do you really want to leave?</h2>
+            <div className={css.modal_buttons}>
+              <button type="button" className={css.modal_cancel_btn}>
+                Cancel
+              </button>
+              <button type="submit" className={css.modal_logout_btn}>
+                Log out
+              </button>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default UserLogoutModal;
