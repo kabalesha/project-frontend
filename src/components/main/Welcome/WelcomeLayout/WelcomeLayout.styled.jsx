f5bb25545b3a0436757e56f4555ac37957d655fc
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-11-22 15:02:56 +0100

    fix welcome page

--- src/components/main/Welcome/WelcomeLayout/WelcomeLayout.styled.jsx
+++ src/components/main/Welcome/WelcomeLayout/WelcomeLayout.styled.jsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import bg from '../../../../images/BgBottleDesk.png';
 import bgTablet from '../../../../images/BgBottleTab.png';
 import bgMobile from '../../../../images/BgBottleMob.png';
-// import bb from '../../../../images/bubblesDesc.png';
-// import bbTablet from '../../../../images/bubblesTab.png';
-// import bbMobile from '../../../../images/bubblesMob.png';
+import bb from '../../../../images/bubblesDesc.png';
+import bbTablet from '../../../../images/bubblesTab.png';
+import bbMobile from '../../../../images/bubblesMob.png';
 
 export const WelcomeStyledLayout = styled.section`
   padding-bottom: 40px;
@@ -22,6 +22,24 @@ export const WelcomeStyledLayout = styled.section`
   }
 
   background-image: url(${bgMobile});
+  background-position: center bottom 50%;
+  background-repeat: no-repeat;
+`;
+export const Div = styled.div`
+  background-size: 100%;
+
+  @media screen and (min-width: 1280px) {
+    padding-top: 80px;
+    padding-bottom: 294px;
+    background-image: url(${bb});
+  }
+  @media screen and (min-width: 480px) and (max-width: 1200px) {
+    background-image: url(${bbTablet});
+    padding-bottom: 50px;
+    /* background-size: auto; */
+  }
+
+  background-image: url(${bbMobile});
   background-position: bottom;
   background-repeat: no-repeat;
 `;
