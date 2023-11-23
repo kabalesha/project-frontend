152104941866b083150730687c892ad5b1662294
Iryna Husieva <02guseva02@gmail.com>
2023-11-23 22:38:17 +0100

    styles

--- src/components/main/Welcome/WelcomeLayout/WelcomeLayout.styled.jsx
+++ src/components/main/Welcome/WelcomeLayout/WelcomeLayout.styled.jsx
@@ -1,45 +1,77 @@
 import styled from 'styled-components';
-import bg from '../../../../images/BgBottleDesk.png';
-import bgTablet from '../../../../images/BgBottleTab.png';
-import bgMobile from '../../../../images/BgBottleMob.png';
-import bb from '../../../../images/bubblesDesc.png';
-import bbTablet from '../../../../images/bubblesTab.png';
-import bbMobile from '../../../../images/bubblesMob.png';
+
+import bbMobile from '../../../../images/bubblesHomeMob.png';
+import bbDesc from '../../../../images/bubblesDesc.png';
+
+import buttleMob from '../../../../images/mobileFrame.png'
+import buttleTab from '../../../../images/backgroundHomeTab.png';
+import buttleDesc from '../../../../images/backgroundHomeDesk.png';
+
 
 export const WelcomeStyledLayout = styled.section`
-  padding-bottom: 40px;
-  background-size: 100%;
-
-  @media screen and (min-width: 1280px) {
-    padding-top: 80px;
-    padding-bottom: 294px;
-    background-image: url(${bg});
-  }
-  @media screen and (min-width: 480px) and (max-width: 1200px) {
-    background-image: url(${bgTablet});
-    padding-bottom: 50px;
-    /* background-size: auto; */
-  }
-
-  background-image: url(${bgMobile});
-  background-position: center bottom 50%;
-  background-repeat: no-repeat;
-`;
-export const Div = styled.div`
-  background-size: 100%;
-
-  @media screen and (min-width: 1280px) {
-    padding-top: 80px;
-    padding-bottom: 294px;
-    background-image: url(${bb});
-  }
-  @media screen and (min-width: 480px) and (max-width: 1200px) {
-    background-image: url(${bbTablet});
-    padding-bottom: 50px;
-    /* background-size: auto; */
-  }
+  width: 100vw; 
+  margin: auto;
+  /* padding-bottom: 40px; */
 
   background-image: url(${bbMobile});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  @media screen and (min-width: 768px) {
+  width: 100vw;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 100vw;
+    
+    background-image: url(${bbDesc});
+    background-size: contain;
+    background-position: center bottom 77%;
+    
+  }
+
+`;
+export const Wrapper = styled.div`
+ width: 280px;
+ padding: 0 20px; 
+
+ @media screen and (min-width: 768px) {
+  width: 768px;
+  
+
+ }
+
+ @media screen and (min-width: 1440px) {
+  width: 1066px;
+  /* padding: 0 32px; */
+  margin-right: auto;
+  margin-left: auto;
+ }
+`;
+
+export const Div = styled.div`
+  background-size: 100%;
+  background-image: url(${buttleMob});
   background-position: bottom;
   background-repeat: no-repeat;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left:20px;
+  
+  @media screen and (min-width: 768px) {
+    background-image: url(${buttleTab});
+    padding-bottom: 90px;
+    background-size: cover;
+    padding-left: 35px;
+  }
+
+  @media screen and (min-width: 1400px) {
+    padding-top: 45px;
+    /* padding-left: 10px; */
+    padding-bottom: 340px;
+    background-image: url(${buttleDesc});
+    background-size: 100%;
+    background-position: center bottom 10%;
+    
+  }
 `;
